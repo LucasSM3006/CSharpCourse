d76e993042333e2f3705040bdc6f0f12c860a054
LucasSM3006 <sasso3006@gmail.com>
2025-01-19 19:05:43 -0300

    Operators in CS

---
+++ Operators/Operators/Program.cs
@@ -0,0 +1,24 @@
+﻿int a = 10;
+int b = 5;
+++a;
+--b;
+a++;
+b--;
+
+Console.WriteLine(a + b);
+Console.WriteLine(a - b);
+Console.WriteLine(a * b);
+Console.WriteLine(a / b);
+Console.WriteLine(a % b);
+
+Console.WriteLine("Addition: " + (a + b));
+Console.WriteLine("Subtraction: " + (a - b));
+Console.WriteLine("Multiplication: " + (a * b));
+Console.WriteLine("Division: " + (a / b));
+Console.WriteLine("Remainder: " + (a % b));
+
+Console.WriteLine("String" + "Concatenation" + ".");
+string word = "abc";
+Console.WriteLine(word + "def");
+
+Console.ReadKey();
