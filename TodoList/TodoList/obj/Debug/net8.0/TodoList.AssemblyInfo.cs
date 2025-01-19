497fef7b9154e7e1f38df66a9c9a38d3584cce2e
LucasSM3006 <sasso3006@gmail.com>
2025-01-19 19:51:42 -0300

    Start of TodoList

--- TodoList/TodoList/obj/Debug/net8.0/TodoList.AssemblyInfo.cs
+++ TodoList/TodoList/obj/Debug/net8.0/TodoList.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TodoList")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d76e993042333e2f3705040bdc6f0f12c860a054")]
 [assembly: System.Reflection.AssemblyProductAttribute("TodoList")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TodoList")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
