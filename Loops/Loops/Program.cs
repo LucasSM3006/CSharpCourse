4697aa98971d69ca9f660704d859acbe02eee6af
LucasSM3006 <sasso3006@gmail.com>
2025-01-20 18:20:14 -0300

    While loop.

---
+++ Loops/Loops/Program.cs
@@ -0,0 +1,11 @@
+﻿int num = 0;
+
+while (num < 10)
+{
+    num++;
+    Console.WriteLine($"Number's current value is: {num}");
+}
+
+Console.WriteLine($"Number's final value: {num}");
+
+Console.ReadKey();
