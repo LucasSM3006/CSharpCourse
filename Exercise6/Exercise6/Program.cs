587b561f0f0ecb3ebef1fd2f663621b385991e2e
LucasSM3006 <sasso3006@gmail.com>
2025-01-20 17:58:21 -0300

    Exercise6, DescribeDay. Given a day of the week as a number, returns either a working day or the weekend.

--- Exercise6/Exercise6/Program.cs
+++ Exercise6/Exercise6/Program.cs
@@ -1,2 +1,37 @@
-﻿.3// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+ * Switch statement - DescribeDay
+
+Implement the DescribeDay method, which given a day of the week as a number, should return:
+
+    "Working day" for numbers 1 to 5
+
+    "Weekend" for numbers 6 and 7
+
+    "Invalid day number" for any other number
+*/
+
+using System;
+
+namespace Coding.Exercise
+{
+    public class Exercise
+    {
+        public static string DescribeDay(int dayNumber)
+        {
+            switch (dayNumber)
+            {
+                case 1:
+                case 2:
+                case 3:
+                case 4:
+                case 5:
+                    return "Working day";
+                case 6:
+                case 7:
+                    return "Weekend";
+                default:
+                    return "Invalid day number";
+            }
+        }
+    }
+}
