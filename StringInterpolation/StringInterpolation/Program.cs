27b8a4d9adde2ce218f10a7ea320959db7139ddc
LucasSM3006 <sasso3006@gmail.com>
2025-01-20 17:34:28 -0300

    String interpolation, aka, embedding variables into a string.

---
+++ StringInterpolation/StringInterpolation/Program.cs
@@ -0,0 +1,11 @@
+﻿// Traditional method I've been doing so far.
+int a = 4, b = 2, c = 10;
+Console.WriteLine(
+    "First is: " + a + ", second is: " + b + ", third is: " + c);
+
+// String interpolation method, aka, embedding strings and variables directly into it.
+Console.WriteLine($"First is: {a}, second is: {b}, third is: {c}");
+
+// Same result, but one feels more natural to write.
+
+Console.ReadKey();
