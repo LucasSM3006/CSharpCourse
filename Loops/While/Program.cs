7e79d6d9d3c90c3db772de37562780c50ddc1d1a
LucasSM3006 <sasso3006@gmail.com>
2025-01-20 18:39:09 -0300

    Loop with user input.

---
+++ Loops/While/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Enter a word");
+string userInput = Console.ReadLine();
+
+while (userInput.Length < 15)
+{
+    userInput += "a";
+    Console.WriteLine(userInput);
+}
+
+Console.WriteLine("Loop is finished");
+Console.ReadKey();
