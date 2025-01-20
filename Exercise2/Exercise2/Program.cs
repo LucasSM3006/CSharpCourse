94bc49c61deb9e4299715ab9cdc4a3383446d68c
LucasSM3006 <sasso3006@gmail.com>
2025-01-19 21:35:32 -0300

    Second exercise using booleans.

--- Exercise2/Exercise2/Program.cs
+++ Exercise2/Exercise2/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+/*
+ * To the isSumLargerOrEqualTo17 variable, assign true if the sum of a and b is larger or equal to 17 and false otherwise.
+ */
+
+namespace Coding.Exercise
+{
+    public class Exercise
+    {
+        public static bool BooleanTypesAndOperators()
+        {
+            int a = 5;
+            int b = 12;
+
+            bool isSumLargerOrEqualTo17 = ((a + b) >= 17);
+
+
+            return isSumLargerOrEqualTo17;
+        }
+    }
+}
