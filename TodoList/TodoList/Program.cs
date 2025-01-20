a6aeef6aa12bdb8363c071c92be6c898a9b379b8
LucasSM3006 <sasso3006@gmail.com>
2025-01-19 22:09:57 -0300

    void method for printing

--- TodoList/TodoList/Program.cs
+++ TodoList/TodoList/Program.cs
@@ -1,3 +1,27 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.ReadKey();
+
+
+Console.WriteLine("Hello, what would you like to do?");
+Console.WriteLine("[S]ee all TODOs");
+Console.WriteLine("[A]dd a TODO");
+Console.WriteLine("[R]emove a TODO");
+Console.WriteLine("[E]xit");
+
+string userChoice = Console.ReadLine();
+
+if(userChoice == "S") {
+    printSelectedOption("See all TODOs");
+} else if (userChoice == "A") {
+    printSelectedOption("Add a TODO");
+} else if (userChoice == "R") {
+    printSelectedOption("Remove a TODO");
+} else if (userChoice == "E") {
+    printSelectedOption("Exit");
+} else {
+    printSelectedOption("Invalid option.");
+}
+
+void printSelectedOption(string selectedOption)
+{
+    Console.WriteLine("Selected option: " + selectedOption);
+}
