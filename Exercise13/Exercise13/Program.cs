472f7f9d60d5b5090db1102a68a32f52ab321bb3
LucasSM3006 <sasso3006@gmail.com>
2025-01-21 20:09:43 -0300

    Solution.

--- Exercise13/Exercise13/Program.cs
+++ Exercise13/Exercise13/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+ * Foreach loop - IsAnyWordLongerThan
+
+Using the foreach loop, implement the IsAnyWordLongerThan method, which given a length and an array of words, returns true if any of those words is longer than the given length, and false otherwise.
+
+
+For example:
+
+    for length 4 and words {"aaa", "bbbb", "ccccc"} the result shall be true, because "ccccc" is longer than 4 letters
+
+    for length 5 and words {"aaa", "bbbb", "ccccc"} the result shall be false because no word is longer than 5 letters
+
+    for length 5 and an empty array, the result shall be false because no word is longer than 5 letters
+ */
+
+using System;
+
+namespace Coding.Exercise
+{
+    public class Exercise
+    {
+        public static bool IsAnyWordLongerThan(int length, string[] words)
+        {
+            foreach (string word in words)
+            {
+                if (word.Length > length) return true;
+            }
+
+            return false;
+        }
+    }
+}
