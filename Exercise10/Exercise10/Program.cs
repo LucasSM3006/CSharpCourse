b11da3400d463c2ad9f160972bf26fcddc6d3695
LucasSM3006 <sasso3006@gmail.com>
2025-01-21 19:18:30 -0300

    Code for the solution

--- Exercise10/Exercise10/Program.cs
+++ Exercise10/Exercise10/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+ * Arrays (1) - BuildHelloString
+
+The purpose of the BuildHelloString is to iterate an array of chars consisting of all letters of the word "hello" and add those letters to the result string variable, so at the end of this method, this variable is equal to "hello".
+
+ */
+
+using System;
+
+namespace Coding.Exercise
+{
+    public class Exercise
+    {
+        public static string BuildHelloString()
+        {
+            char[] letters = new char[] { 'h', 'e', 'l', 'l', 'o' };
+            string result = "";
+            for (int i = 0; i < letters.Length; ++i)
+            {
+                result += letters[i];
+            }
+            return result;
+        }
+    }
+}
+
