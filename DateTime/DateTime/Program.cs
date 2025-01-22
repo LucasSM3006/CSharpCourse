cb1f034379191e6c09770fe55ce6afcdee44a3ff
LucasSM3006 <sasso3006@gmail.com>
2025-01-22 17:00:30 -0300

    DateTime class and a few of its useful methods.

---
+++ DateTime/DateTime/Program.cs
@@ -0,0 +1,18 @@
+﻿var internationalPizzaDay25 = new DateTime(2025, 2, 9);
+var today = new DateTime(2025, 1, 22);
+
+Console.WriteLine($"Year is: {internationalPizzaDay25.Year}");
+Console.WriteLine($"Month is: {internationalPizzaDay25.Month}");
+Console.WriteLine($"Day is: {internationalPizzaDay25.Day}");
+Console.WriteLine($"Day of the week will be: {internationalPizzaDay25.DayOfWeek}");
+
+Console.WriteLine();
+
+var internationalPizzaDay35 = internationalPizzaDay25.AddYears(10);
+
+Console.WriteLine($"Year is: {internationalPizzaDay35.Year}");
+Console.WriteLine($"Month is: {internationalPizzaDay35.Month}");
+Console.WriteLine($"Day is: {internationalPizzaDay35.Day}");
+Console.WriteLine($"Day of the week will be: {internationalPizzaDay35.DayOfWeek}");
+
+Console.ReadKey();
