76e43e9e641a7455e649918430559373f41a1e52
LucasSM3006 <sasso3006@gmail.com>
2025-01-22 19:09:36 -0300

    Overloading of methods, aka, methods with the same name but different parameters.

---
+++ MethodOverloading/MethodOverloading/Program.cs
@@ -0,0 +1,30 @@
+﻿MedicalAppointment appointment = new MedicalAppointment("Johnathan Joestar", new DateTime(2025, 4, 3));
+
+// Overwrite entire appointment date
+appointment.reschedule(new DateTime(2025, 4, 3));
+
+// Overwrite month and day
+appointment.reschedule(12, 2);
+
+
+public class MedicalAppointment
+{
+    private string _patientName;
+    private DateTime _date;
+
+    public MedicalAppointment(string patientName, DateTime date)
+    {
+        _patientName = patientName;
+        _date = date;
+    }
+
+    public void reschedule(DateTime date)
+    {
+        _date = date;
+    }
+
+    public void reschedule(int month, int day)
+    {
+        _date = new DateTime(_date.Year, month, day);
+    }
+}
