14d05178c1ab975ab6ade696769620d4e34e4b6d
LucasSM3006 <sasso3006@gmail.com>
2025-01-22 00:18:55 -0300

    Out keyword, so you can manipulate and 'return' multiple values. (You're still only returning one value, but manipulating the actual variable instead of a copy)

---
+++ OutKeyword/OutKeyword/Program.cs
@@ -0,0 +1,26 @@
+﻿int[] numbers = new [] { 10, -8, 2, 12, -17 };
+int nonPositiveCount;
+List<int> positiveNumbers = GetOnlyPositive(numbers, out nonPositiveCount);
+
+Console.WriteLine($"This many non positive numbers: {nonPositiveCount}");
+
+Console.ReadKey();
+
+List<int> GetOnlyPositive(int[] numbers, out int countNonPositive)
+{
+    List<int> result = new List<int>();
+    countNonPositive = 0;
+
+    foreach (int number in numbers)
+    {
+        if(number > 0)
+        {
+            result.Add(number);
+        }
+        else
+        {
+            countNonPositive++;
+        }
+    }
+    return result;
+}
