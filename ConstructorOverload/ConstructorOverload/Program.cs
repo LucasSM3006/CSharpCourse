b7d45c116747515facabc849840819ffd2f0f6e3
LucasSM3006 <sasso3006@gmail.com>
2025-01-22 19:22:09 -0300

    Overloading the constructor. Also shows how to call a constructor from a constructor.

---
+++ ConstructorOverload/ConstructorOverload/Program.cs
@@ -0,0 +1,49 @@
+﻿MedicalAppointment appointment = new MedicalAppointment("Johnathan Joestar", new DateTime(2025, 4, 3));
+
+// Overwrite entire appointment date
+appointment.reschedule(new DateTime(2025, 4, 3));
+
+// Overwrite month and day
+appointment.overwriteMonthAndDay(12, 2);
+
+
+public class MedicalAppointment
+{
+    private string _patientName;
+    private DateTime _date;
+
+    public MedicalAppointment(string patientName, DateTime date)
+    {
+        _patientName = patientName;
+        _date = date;
+    }
+
+    public MedicalAppointment(string patientName) : this(patientName, 7)
+    {
+
+    }
+
+    public MedicalAppointment(string patientName, int daysFromNow)
+    {
+        _patientName = patientName;
+        _date = DateTime.Now.AddDays(daysFromNow);
+    }
+
+    public void reschedule(DateTime date)
+    {
+        _date = date;
+    }
+
+    public void overwriteMonthAndDay(int month, int day)
+    {
+        _date = new DateTime(_date.Year, month, day);
+    }
+
+    public void moveByMonthsAndDays(int monthsToAdd, int daysToAdd)
+    {
+        _date = new DateTime(
+            _date.Year,
+            _date.Month + monthsToAdd,
+            _date.Day + daysToAdd);
+    }
+}
