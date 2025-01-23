a847faf94bbfeae7808ce3a5a956c8373e761e8b
LucasSM3006 <sasso3006@gmail.com>
2025-01-23 20:45:14 -0300

    Exercise18, properties of the Order class. Explanation in comments. Implemeting ATM.

---
+++ Exercise18/Exercise18/Program.cs
@@ -0,0 +1,29 @@
+﻿/*
+ * Properties of the Order class
+
+The goal of this exercise is to finish the implementation of the Order class. It represents a simple Order made in a store, and it needs to have two properties:
+
+    Item (string), which should not be settable at all
+
+    Date (DateTime), which should be both gettable and settable. Its setter should validate if the given value has the same year as the current year. If not, the value of the Date property should not be changed.
+
+Your job is only to add the definitions of those two properties. If needed, you can add backing fields as well.
+ * 
+ */
+
+using System;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Coding.Exercise
+{
+    public class Order
+    {
+        //your code goes here
+
+        public Order(string item, DateTime date)
+        {
+            Item = item;
+            Date = date;
+        }
+    }
+}
