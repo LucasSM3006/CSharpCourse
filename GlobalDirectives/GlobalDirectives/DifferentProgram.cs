2c357afc69b58facbffeafad0aeb823e24ca1e84
LucasSM3006 <sasso3006@gmail.com>
2025-01-24 20:51:10 -0300

    Global 'using'. Example on how to declare them and commented explanation.

---
+++ GlobalDirectives/GlobalDirectives/DifferentProgram.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// There's no 'using System.Diagnostics;' here because it's declared on Program.cs as a global usage.
+
+public class DifferentProgram
+{
+    static public void DoThat()
+    {
+        var stopWatch = Stopwatch.StartNew();
+
+        for (int i = 0; i< 1000; i++)
+        {
+            Console.WriteLine($"Current iteration: {i}");
+        }
+        stopWatch.Stop();
+
+        Console.WriteLine($"Time in stopwatch (ms): {stopWatch.ElapsedMilliseconds}");
+
+        Console.ReadKey();
+    }
+}
---
+++ GlobalDirectives/GlobalDirectives/Program.cs
@@ -0,0 +1,17 @@
+﻿global using System.Diagnostics;
+// 'Global' using must be above non-global ones. So it needs to go above System.
+
+//var stopWatch = Stopwatch.StartNew();
+
+//for (int i = 0; i < 1000; i++)
+//{
+//    Console.WriteLine($"Current iteration: {i}");
+//}
+
+//stopWatch.Stop();
+
+//Console.WriteLine($"Time in stopwatch (ms): {stopWatch.ElapsedMilliseconds}");
+
+//Console.ReadKey();
+
+DifferentProgram.DoThat();
