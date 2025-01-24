0102437eecad6652b3917cd9673d1d5e106f346f
LucasSM3006 <sasso3006@gmail.com>
2025-01-23 22:43:15 -0300

    Exercise20, static class NumbersToDayOfWeekTranslator

---
+++ Exercise20/Exercise20/Program.cs
@@ -0,0 +1,27 @@
+﻿/*
+ * Static classes - NumberToDayOfWeekTranslator
+
+Implement the static NumberToDayOfWeekTranslator class that can be used like this:
+
+
+
+The Translate method should take a number and return a string with the name of the day of the week, according to the following:
+
+    for 1, "Monday" is returned
+
+    for 2, "Tuesday" is returned
+
+    ...
+
+    for 7, "Sunday" is returned
+
+    for any other number, "Invalid day of the week" is returned
+ * 
+ */
+
+using System;
+
+namespace Coding.Exercise
+{
+    // Your code here
+}
