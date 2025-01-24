96a4c2d6def932925b4e06ae8e25cc5616616e0a
LucasSM3006 <sasso3006@gmail.com>
2025-01-23 21:47:29 -0300

    Initial code, comments explaining things. First version has an example of it being initialized.

---
+++ ObjectInitializers/ObjectInitializers/Program.cs
@@ -0,0 +1,29 @@
+﻿// Important notice: Object initializers need the setters to work.
+// This isn't good because you can manipulate it at any point with no validations.
+// So we have the 'init' keyword. The version w/ the init keyword will be in the latest commit.
+
+//Person person = new Person("John Doe", 2001);
+Person person = new Person("John Doe")
+{
+    Name = "Jane Doe", // Jane doe will be the name used. Constructor is called first, and then the Object initializer does its job, overwriting Name.
+    YearOfBirth = 2001
+};
+
+Console.ReadKey();
+
+class Person
+{
+    public string Name { get; set; }
+    public int YearOfBirth { get; set; }
+
+    public Person(string name)
+    {
+        Name = name;
+    }
+
+    //public Person(string name, int yearOfBirth)
+    //{
+    //    Name = name;
+    //    YearOfBirth = yearOfBirth;
+    //}
+}
