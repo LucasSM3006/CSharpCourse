c9979571af1b1698e118500ac5c765dbd3ad0dfb
LucasSM3006 <sasso3006@gmail.com>
2025-01-25 20:26:28 -0300

    Last but not least, DiceGame was also moved into the new folder. Thus finishing the refactory, everything having been moved into their own classes.

---
+++ DiceRollGameAssignment/DiceRollGameAssignment/Game/DiceGame.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiceRollGameAssignment.Game
+{
+    using DiceRollGameAssignment.Game;
+
+    public class DiceGame
+    {
+        const int NumberOfTries = 3;
+        Random randomNumberGenerator = new Random();
+        GamePrinter gamePrinter = new GamePrinter();
+
+        public GameResult Playgame()
+        {
+
+            Dice dice = new Dice(6, randomNumberGenerator); // Number of sides the dice has.
+            int numberOfSides = dice.GetSides();
+            int randomNumber = dice.RollDice();
+            bool win = false;
+
+            gamePrinter.PrintStartGame($"Dice with {numberOfSides} sides rolled. You have {NumberOfTries} tries to guess the number it landed on.");
+
+            for (int i = 1; i <= NumberOfTries; i++)
+            {
+                Console.WriteLine($"Current trial: {i}");
+
+                int guess = ConsoleReader.ReadInteger("Enter your guess: ");
+
+                if (guess == randomNumber)
+                {
+                    return GameResult.Win;
+                }
+
+                Console.WriteLine("Wrong number.");
+            }
+
+            return GameResult.Loss;
+        }
+    }
+}
--- DiceRollGameAssignment/DiceRollGameAssignment/Program.cs
+++ DiceRollGameAssignment/DiceRollGameAssignment/Program.cs
@@ -8,37 +8,3 @@ GameResult gameResult = diceGame.Playgame();
 gamePrinter.PrintEndGame(gameResult);
 
 Console.ReadKey();
-
-public class DiceGame
-{
-    const int NumberOfTries = 3;
-    Random randomNumberGenerator = new Random();
-    GamePrinter gamePrinter = new GamePrinter();
-
-    public GameResult Playgame()
-    {
-        
-        Dice dice = new Dice(6, randomNumberGenerator); // Number of sides the dice has.
-        int numberOfSides = dice.GetSides();
-        int randomNumber = dice.RollDice();
-        bool win = false;
-
-        gamePrinter.PrintStartGame($"Dice with {numberOfSides} sides rolled. You have {NumberOfTries} tries to guess the number it landed on.");
-
-        for (int i = 1; i <= NumberOfTries; i++)
-        {
-            Console.WriteLine($"Current trial: {i}");
-
-            int guess = ConsoleReader.ReadInteger("Enter your guess: ");
-
-            if (guess == randomNumber)
-            {
-                return GameResult.Win;
-            }
-
-            Console.WriteLine("Wrong number.");
-        }
-
-        return GameResult.Loss;
-    }
-}
