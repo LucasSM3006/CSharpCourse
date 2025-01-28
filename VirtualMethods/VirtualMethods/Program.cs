4c9ce2b5074903062e921b0f125b50d609d35534
LucasSM3006 <sasso3006@gmail.com>
2025-01-27 23:21:49 -0300

    Overriding methods.

--- VirtualMethods/VirtualMethods/Program.cs
+++ VirtualMethods/VirtualMethods/Program.cs
@@ -1,5 +1,17 @@
 ﻿var numbers = new List<int>() { 1, 4, 6, -1, 12, 44, -8, -19 };
-var sum = new NumbersSumCalculator().Calculate(numbers);
+bool shallAddPositiveOnly = true;
+
+int sum;
+
+if(shallAddPositiveOnly)
+{
+    sum = new PositiveNumbersSumCalculator().Calculate(numbers);
+}
+else
+{
+    sum = new NumbersSumCalculator().Calculate(numbers);
+}
+
 Console.WriteLine("Sum is: " + sum);
 Console.ReadKey();
 
@@ -10,8 +22,23 @@ public class NumbersSumCalculator
         int sum = 0;
         foreach (int number in numbers)
         {
-            sum += number;
+            if(ShallBeAdded(number)){
+                sum += number;
+            }
         }
         return sum;
+    }
+
+    protected virtual bool ShallBeAdded(int number)
+    {
+        return true;
+    }
+}
+
+public class PositiveNumbersSumCalculator : NumbersSumCalculator
+{
+    protected override bool ShallBeAdded(int number)
+    {
+        return number > 0;
     }
 }
