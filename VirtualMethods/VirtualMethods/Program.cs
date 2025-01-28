e9971bd5aa17838b554213dc5db6c9676d8ec28d
LucasSM3006 <sasso3006@gmail.com>
2025-01-27 23:10:39 -0300

    Starting class.

---
+++ VirtualMethods/VirtualMethods/Program.cs
@@ -0,0 +1,17 @@
+﻿var numbers = new List<int>() { 1, 4, 6, -1, 12, 44, -8, -19 };
+var sum = new NumbersSumCalculator().Calculate(numbers);
+Console.WriteLine("Sum is: " + sum);
+Console.ReadKey();
+
+public class NumbersSumCalculator
+{
+    public int Calculate(List<int> numbers)
+    {
+        int sum = 0;
+        foreach (int number in numbers)
+        {
+            sum += number;
+        }
+        return sum;
+    }
+}
