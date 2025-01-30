fbccd7eaa032f5e079571819815760a32aa3e082
LucasSM3006 <sasso3006@gmail.com>
2025-01-30 20:24:24 -0300

    Exercise 27, interfaces. Applying multiple transformations to a number.

---
+++ Exercise27/Exercise27/Program.cs
@@ -0,0 +1,100 @@
+﻿/*
+ * Interfaces - Applying multiple transformations to a number
+
+Implement all types necessary to make this code work:
+
+        public static int Transform(
+            int number)
+        {
+            var transformations = new List<INumericTransformation>
+            {
+                new By1Incrementer(),
+                new By2Multiplier(),
+                new ToPowerOf2Raiser()
+            };
+            
+            var result = number;
+            foreach(var transformation in transformations)
+            {
+                result = transformation.Transform(result);
+            }
+            return result;
+        }
+    }
+
+
+You will need to define the INumericTransformation interface and the following classes implementing it:
+
+    By1Incrementer, whose Transform method adds 1 to the input
+
+    By2Multiplier, whose Transform method multiplies input by 2
+
+    ToPowerOf2Raiser, whose Transform method raises input to the power of 2
+
+All those transformations will be applied to the given number, so for example, if the input is 4, the result of the Exercise.Transform method will be 100 because:
+
+    4 will be incremented by 1, giving 5
+
+    5 will be multiplied by 2, giving 10
+
+    10 will be raised to the power of 2, giving 100
+ * 
+ */
+
+int transform = Exercise.Transform(4);
+
+Console.WriteLine(transform);
+
+Console.ReadKey();
+
+public static class Exercise
+{
+    public static int Transform(
+        int number)
+    {
+        var transformations = new List<INumericTransformation>
+            {
+                new By1Incrementer(),
+                new By2Multiplier(),
+                new ToPowerOf2Raiser()
+            };
+
+        var result = number;
+        foreach (var transformation in transformations)
+        {
+            result = transformation.Transform(result);
+        }
+        return result;
+    }
+}
+
+public interface INumericTransformation
+{
+    int Transform(int number);
+}
+
+public class By1Incrementer : INumericTransformation
+{
+    public int Transform(int number)
+    {
+        return number + 1;
+    }
+}
+
+public class By2Multiplier : INumericTransformation
+{
+    public int Transform(int number)
+    {
+        return number * 2;
+    }
+}
+
+public class ToPowerOf2Raiser : INumericTransformation
+{
+    public int Transform(int number)
+    {
+        return number * number;
+    }
+}
+
+//todo your code goes here
