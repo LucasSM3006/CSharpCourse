8fa49e2633d9d86244fde0bc98813a8b6915708a
LucasSM3006 <sasso3006@gmail.com>
2025-01-30 20:48:17 -0300

    Serializing and deserializing JSON. Turns out it's fairly simple.

---
+++ JSON/JSON/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Text.Json;
+
+Person person = new Person
+{
+    firstName = "John",
+    lastName = "Doe",
+    yearOfBirth = 1999
+};
+
+var asJson = JsonSerializer.Serialize(person);
+
+Console.WriteLine("As JSON: ");
+Console.WriteLine(asJson);
+
+var personJson = "{\"firstName\":\"John\",\"lastName\":\"Doe\",\"yearOfBirth\":1999}";
+
+Person personFromJson = JsonSerializer.Deserialize<Person>(personJson);
+
+Console.WriteLine(personFromJson.firstName + " " + personFromJson.lastName + " " + personFromJson.yearOfBirth);
+
+Console.ReadKey();
+
+public class Person
+{
+    public string firstName { get; set; }
+    public string lastName { get; set; }
+    public int yearOfBirth { get; set; }
+}
