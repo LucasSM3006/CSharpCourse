a1da39684d7b403b7a45853a06c6db8e088e52e8
LucasSM3006 <sasso3006@gmail.com>
2025-02-01 17:45:20 -0300

    Moved all ingredients to a new folder and to their own file.

---
+++ CookieCookbookAssignment/CookieCookbookAssignment/Recipes/Ingredients/CocoaPowder.cs
@@ -0,0 +1,9 @@
+﻿namespace CookieCookbookAssignment.Recipes.Ingredients
+{
+    public class CocoaPowder : Ingredient
+    {
+        public override int Id => 8;
+        public override string Name => "Cinnamon";
+
+    }
+}
--- CookieCookbookAssignment/CookieCookbookAssignment/Recipes/Recipe.cs
+++ CookieCookbookAssignment/CookieCookbookAssignment/Recipes/Recipe.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CookieCookbookAssignment.Recipes.Ingredients;
 
 namespace CookieCookbookAssignment.Recipes
 {
@@ -14,82 +15,5 @@ namespace CookieCookbookAssignment.Recipes
         {
             Ingredients = ingredients;
         }
-    }
-
-    public abstract class Ingredient
-    {
-        public abstract int Id { get; }
-        public abstract string Name { get; }
-        public virtual string PreparationInstructions => "Add to other ingredients.";
-    }
-
-    public abstract class Flour : Ingredient
-    {
-        public override string PreparationInstructions => $"Sieve. {base.PreparationInstructions}";
-    }
-
-    public class WheatFlour : Flour
-    {
-        public override int Id => 1;
-
-        public override string Name => "Wheat Flour";
-    }
-
-    public class CoconutFlour : Flour
-    {
-        public override int Id => 2;
-
-        public override string Name => "Coconut flour";
-    }
-
-    public class Butter : Ingredient
-    {
-        public override int Id => 3;
-
-        public override string Name => "Butter";
-
-        public override string PreparationInstructions => $"Melt. {base.PreparationInstructions}";
-    }
-
-    public class Chocolate : Ingredient
-    {
-        public override int Id => 4;
-
-        public override string Name => "Chocolate";
-
-        public override string PreparationInstructions => $"Melt in a water bath. {base.PreparationInstructions}";
-    }
-
-    public class Sugar : Ingredient
-    {
-        public override int Id => 5;
-
-        public override string Name => "Sugar";
-    }
-
-    public abstract class Spice : Ingredient
-    {
-        public override string PreparationInstructions => $"Take half a teaspoon. {base.PreparationInstructions}";
-    }
-
-    public class Cardamon : Spice
-    {
-        public override int Id => 6;
-
-        public override string Name => "Cardamon";
-    }
-
-    public class Cinnamon : Spice
-    {
-        public override int Id => 7;
-
-        public override string Name => "Cinnamon";
-    }
-
-    public class CocoaPowder : Ingredient
-    {
-        public override int Id => 8;
-        public override string Name => "Cinnamon";
-
     }
 }
