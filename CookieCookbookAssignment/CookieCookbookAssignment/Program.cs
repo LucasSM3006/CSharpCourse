fbd2e093a42b4047dadddd22bf0c7f4159aee492
LucasSM3006 <sasso3006@gmail.com>
2025-02-01 16:56:11 -0300

    Redoing the app, starting with the flow. Things like prompting the user, exiting, creating, will all be handled by a different class. It was what I planned from the start with the other one, but kept everything in the main flow class, thus confusing myself and making the process harder later on when I had to continue.

--- CookieCookbookAssignment/CookieCookbookAssignment/Program.cs
+++ CookieCookbookAssignment/CookieCookbookAssignment/Program.cs
@@ -1,1 +1,32 @@
-﻿
+﻿CookiesRecipeApplication app = new CookiesRecipeApplication();
+app.Run();
+
+public class CookiesRecipeApplication
+{
+    private readonly RecipesRepository _recipesRepository;
+    private readonly RecipesUserInteraction _recipesUserInteraction;
+
+    public CookiesRecipeApplication(RecipesRepository recipesRepository, RecipesUserInteraction recipesUserInteraction)
+    {
+        _recipesRepository = recipesRepository;
+        _recipesUserInteraction = recipesUserInteraction;
+    }
+
+    public void Run()
+    {
+        RecipesRepository allRecipes = _recipesRepository.Load();
+        _recipesUserInteraction.PrintExistingRecipes(allRecipes);
+    }
+}
+
+
+
+public class Recipe
+{
+
+}
+
+public class Ingredient
+{
+
+}
