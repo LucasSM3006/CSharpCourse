837573d5228bf903280fbe3b5b7517fb7e26a21b
LucasSM3006 <sasso3006@gmail.com>
2025-02-01 18:10:20 -0300

    Commented out bits that don't compile just to test it out.

--- CookieCookbookAssignment/CookieCookbookAssignment/Program.cs
+++ CookieCookbookAssignment/CookieCookbookAssignment/Program.cs
@@ -1,9 +1,15 @@
-﻿RecipesConsoleUserInteraction console = new RecipesConsoleUserInteraction();
+﻿using CookieCookbookAssignment.Recipes;
+using CookieCookbookAssignment.Recipes.Ingredients;
+using System.Diagnostics.Metrics;
+
+RecipesConsoleUserInteraction console = new RecipesConsoleUserInteraction();
 TextRecipesRepository repository = new TextRecipesRepository();
 
 CookiesRecipeApplication app = new CookiesRecipeApplication(repository, console);
 
-app.Run();
+
+
+app.Run("recipes.txt");
 
 public class CookiesRecipeApplication
 {
@@ -16,30 +22,30 @@ public class CookiesRecipeApplication
         _recipesUserInteraction = recipesUserInteraction;
     }
 
-    public void Run()
+    public void Run(string filePath)
     {
-        IRecipesRepository allRecipes = _recipesRepository.Load();
+        List<Recipe> allRecipes = _recipesRepository.Load();
         _recipesUserInteraction.PrintExistingRecipes(allRecipes);
 
         _recipesUserInteraction.PromptToCreateRecipes();
 
-        List<Ingredient> ingredients = _recipesUserInteraction.ReadIngredientsFromUser();
+        //List<Ingredient> ingredients = _recipesUserInteraction.ReadIngredientsFromUser();
 
-        if(ingredients.Count > 0)
-        {
-            Recipe recipe = new Recipe(ingredients);
+        //if(ingredients.Count > 0)
+        //{
+        //    Recipe recipe = new Recipe(ingredients);
 
-            allRecipes.Add(recipe);
+        //    allRecipes.Add(recipe);
 
-            _recipesRepository.Write(filePath, allRecipes);
+        //    _recipesRepository.Write(filePath, allRecipes);
 
-            _recipesUserInteraction.ShowMessage("Recipe added: ");
-            _recipesUserInteraction.ShowMessage(recipe.ToString());
-        }
-        else
-        {
-            _recipesUserInteraction.ShowMessage("No ingredients added, recipe won't be saved.");
-        }
+        //    _recipesUserInteraction.ShowMessage("Recipe added: ");
+        //    _recipesUserInteraction.ShowMessage(recipe.ToString());
+        //}
+        //else
+        //{
+        //    _recipesUserInteraction.ShowMessage("No ingredients added, recipe won't be saved.");
+        //}
 
         _recipesUserInteraction.Exit();
     }
@@ -49,12 +55,34 @@ public interface IRecipesUserInteraction
 {
     public void ShowMessage(string message);
     public void PromptToCreateRecipes();
-    public void PrintExistingRecipes(List<Recipe> recipes);
+    public void PrintExistingRecipes(IEnumerable<Recipe> recipes); // When in doubt, pick a general type...
     public void Exit();
+}
+
+public class IngredientsRegister
+{
+    public IEnumerable<Ingredient> All { get; } = new List<Ingredient>
+    {
+        new WheatFlour(),
+        new CoconutFlour(),
+        new Butter(),
+        new Chocolate(),
+        new Sugar(),
+        new Cardamon(),
+        new Cinnamon(),
+        new CocoaPowder()
+    };
 }
 
 public class RecipesConsoleUserInteraction : IRecipesUserInteraction
 {
+    private readonly IngredientsRegister _ingredientsRegister;
+
+    public RecipesConsoleUserInteraction(IngredientsRegister ingredientsRegister)
+    {
+        _ingredientsRegister = ingredientsRegister;
+    }
+
     public void ShowMessage(string message)
     {
         Console.WriteLine(message);
@@ -62,12 +90,30 @@ public class RecipesConsoleUserInteraction : IRecipesUserInteraction
 
     public void PromptToCreateRecipes()
     {
+        Console.WriteLine("Create a cookie recipe! Available ingredients are:");
 
+        foreach(Ingredient ingredient in _ingredientsRegister.All)
+        {
+            Console.WriteLine($"{ingredient.Id}. {ingredient.Name}");
+        }
     }
 
-    public void PrintExistingRecipes(List<Recipe> recipes)
+    public void PrintExistingRecipes(IEnumerable<Recipe> recipes)
     {
+        if(recipes.Count() > 0)
+        {
+            Console.WriteLine("Existing recipes are: " + Environment.NewLine); // Newline's better to use than '\n'. Apparently it can cause issues due to different OS's.
 
+            int counter = 0;
+
+            foreach(Recipe recipe in recipes)
+            {
+                Console.WriteLine($"*****{counter + 1}*****");
+                Console.WriteLine(recipe);
+                Console.WriteLine();
+                counter++;
+            }
+        }
     }
 
     public void Exit()
@@ -90,9 +136,32 @@ public class TextRecipesRepository : IRecipesRepository
 
     }
 
+    // Dummy method for testing. Will implement later.
     public List<Recipe> Load()
     {
-        throw new NotImplementedException();
+        return new List<Recipe>
+        {
+            new Recipe(new List<Ingredient>
+            {
+                new CocoaPowder(),
+                new Butter(),
+                new Sugar()
+            }),
+            new Recipe(new List<Ingredient>
+            {
+                new Chocolate(),
+                new Sugar(),
+                new Cinnamon()
+            }),
+            new Recipe(new List<Ingredient>
+            {
+                new Sugar(),
+                new Sugar(),
+                new Chocolate(),
+                new Butter(),
+                new Cinnamon()
+            })
+        };
     }
 }
 
