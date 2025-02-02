4d597322289d7f565c2d56cf327f51cc98f0738f
LucasSM3006 <sasso3006@gmail.com>
2025-02-01 21:39:16 -0300

    Handles all file accesses. Holds the metadata, the filetype enum, and the extensions.

---
+++ CookieCookbookAssignment/CookieCookbookAssignment/FileAccess/FileTypeExtensions.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CookieCookbookAssignment.FileAccess;
+
+public static class FileTypeExtensions
+{
+    public static string AsFileExtension(this FileType fileFormat) =>
+        fileFormat == FileType.Json ? "json" : "txt";
+}
