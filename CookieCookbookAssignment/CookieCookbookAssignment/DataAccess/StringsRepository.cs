12aaf48bb2bb68e475d106f2d422bae77a193ad0
LucasSM3006 <sasso3006@gmail.com>
2025-02-01 21:38:22 -0300

    Data access folder, handles all data access related classes, string repository, textual, json, etc. Can be reused on other apps.

---
+++ CookieCookbookAssignment/CookieCookbookAssignment/DataAccess/StringsRepository.cs
@@ -0,0 +1,23 @@
+﻿namespace CookieCookbookAssignment.DataAccess;
+
+public abstract class StringsRepository : IStringsRepository
+{
+    public List<string> Read(string filename)
+    {
+        if (File.Exists(filename))
+        {
+            var fileContents = File.ReadAllText(filename);
+            return TextToStrings(fileContents);
+        }
+        return new List<string>();
+    }
+
+    protected abstract List<string> TextToStrings(string fileContents);
+
+    public void Write(string filePath, List<string> strings)
+    {
+        File.WriteAllText(filePath, StringsToText(strings));
+    }
+
+    protected abstract string StringsToText(List<string> strings);
+}
