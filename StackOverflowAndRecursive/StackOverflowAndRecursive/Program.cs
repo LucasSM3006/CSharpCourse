d1d818bedc692816faef9d77400af47007d4c3ec
LucasSM3006 <sasso3006@gmail.com>
2025-02-03 20:25:22 -0300

    A way to stop the recusrive method.

--- StackOverflowAndRecursive/StackOverflowAndRecursive/Program.cs
+++ StackOverflowAndRecursive/StackOverflowAndRecursive/Program.cs
@@ -1,7 +1,12 @@
-﻿// RecursiveMethod(); // It'll take a bit but it will eventually throw an error.
+﻿RecursiveMethod(1); // It'll take a bit but it will eventually throw an error.
 
-void RecursiveMethod()
+Console.ReadKey();
+
+void RecursiveMethod(int counter)
 {
-    Console.WriteLine("Calling myself.");
-    RecursiveMethod();
+    Console.WriteLine($"Calling myself. Counter value: {counter}");
+    if ( counter < 10 ) // Have some way to stop it, otherwise you'll be toast.
+    {
+        RecursiveMethod(++counter); // Cannot be 'counter++'. It won't work.
+    }
 }
