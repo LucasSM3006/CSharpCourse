7cb7a9935302c10454a7a8bbb97e61db9e0cf3e4
LucasSM3006 <sasso3006@gmail.com>
2025-02-03 20:13:40 -0300

    More exaamples of exceptions.

--- ExplicitExceptions/ExplicitExceptions/Program.cs
+++ ExplicitExceptions/ExplicitExceptions/Program.cs
@@ -3,7 +3,22 @@
 
 Person invalidPerson = new Person("", -100);
 
+// int[] numbers = new int[] { 1, 2, 3 };
+// int fourthNumber = numbers[4]; // Index out of range exception.
+
+List<int> emptyCollection = new List<int>();
+int firstElement = GetFirstElement(emptyCollection);
+int firstElementUsingLINQ = emptyCollection.First(); // This throws an 'InvalidOperationException' on the other hand.
+
+string passport = "Czech";
+bool passportValid = ValidatePassport(passport);
+
 Console.ReadKey();
+
+bool ValidatePassport(string passport)
+{
+    throw new NotImplementedException(); // Use this to make it compile, but shows it's still WIP.
+}
 
 int GetFirstElement(IEnumerable<int> numbers)
 {
@@ -12,7 +27,7 @@ int GetFirstElement(IEnumerable<int> numbers)
         return number;
     }
 
-    throw new Exception("The collection cannot be empty.");
+    throw new InvalidOperationException("The collection cannot be empty.");
     // This is okay because it's not this method's responsibility to figure out what to do with the invalid input.
     // We throw an exception here because we can't handle the input in a reasonable way, and an invalid input is whoever's implementing the method's mistake.
 }
@@ -24,13 +39,17 @@ class Person
 
     public Person(string name, int yearOfBirth)
     {
+        if (name is null)
+        {
+            throw new ArgumentNullException("Name cannot be null.");
+        }
         if (name == string.Empty)
         {
-            throw new Exception("Invalid name. Must not be empty.");
+            throw new ArgumentException("Invalid name. Must not be empty.");
         }
         if (yearOfBirth < 1900 || yearOfBirth > DateTime.Now.Year)
         {
-            throw new Exception("Invalid year. Must be below current year & above 1900");
+            throw new ArgumentOutOfRangeException("Invalid year. Must be below current year & above 1900");
         }
         Name = name;
         YearOfBirth = yearOfBirth;
