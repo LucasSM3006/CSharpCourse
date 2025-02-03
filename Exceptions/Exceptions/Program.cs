86cf4cc2b90ffbf0872f0db7b5de7c76a58a8790
LucasSM3006 <sasso3006@gmail.com>
2025-02-03 18:47:45 -0300

    Try-catch-finally block.

--- Exceptions/Exceptions/Program.cs
+++ Exceptions/Exceptions/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Enter a number:");
+string input = Console.ReadLine();
+try
+{
+    int number = ParseStringToInt(input);
+    Console.WriteLine($"String successfully parsed. Result is: {number}");
+}
+catch
+{
+    Console.WriteLine("An exception was thrown.");
+}
+finally
+{
+    Console.WriteLine("Finally block being executed");
+}
+
+Console.ReadKey();
+
+int ParseStringToInt(string input)
+{
+    return int.Parse(input);
+}
