1ac491adfd5f65ffd5e47a53566c034c7abed512
LucasSM3006 <sasso3006@gmail.com>
2025-02-03 19:52:18 -0300

    Exception thrown, and explanation as to why it is okay.

--- ExplicitExceptions/ExplicitExceptions/Program.cs
+++ ExplicitExceptions/ExplicitExceptions/Program.cs
@@ -1,4 +1,7 @@
-﻿Console.ReadKey();
+﻿// List<int> numbers = new List<int> { 1, 2, 3 };
+// int first = GetFirstElement(numbers);
+
+Console.ReadKey();
 
 int GetFirstElement(IEnumerable<int> numbers)
 {
@@ -6,5 +9,20 @@ int GetFirstElement(IEnumerable<int> numbers)
     {
         return number;
     }
-    return -1;
+
+    throw new Exception("The collection cannot be empty.");
+    // This is okay because it's not this method's responsibility to figure out what to do with the invalid input.
+    // We throw an exception here because we can't handle the input in a reasonable way, and an invalid input is whoever's implementing the method's mistake.
+}
+
+class Person
+{
+    public string Name { get; }
+    public int YearOfBirth { get; }
+
+    public Person(string name, int yearOfBirth)
+    {
+        Name = name;
+        YearOfBirth = yearOfBirth;
+    }
 }
