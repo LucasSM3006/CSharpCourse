81f5187bd8c316a6633d332b2d5b64781b39f3c6
LucasSM3006 <sasso3006@gmail.com>
2025-02-03 19:12:21 -0300

    Added another catch block to handle division by zero.

--- Exceptions/Exceptions/Program.cs
+++ Exceptions/Exceptions/Program.cs
@@ -9,7 +9,12 @@ try
 catch (FormatException e)
 {
     Console.WriteLine("Wrong format. Input is not parsable into an integer.");
-    Console.WriteLine($"Exception message: {e.Message}");
+    Console.WriteLine(GetExceptionMessage(e));
+}
+catch (DivideByZeroException e)
+{
+    Console.WriteLine("Cannot divide by zero. Input cannot be zero.");
+    Console.WriteLine(GetExceptionMessage(e));
 }
 finally // Always executed even when it runs properly. Useful for closing DB connections, for example.
 {
@@ -21,4 +26,9 @@ Console.ReadKey();
 int ParseStringToInt(string input)
 {
     return int.Parse(input);
+}
+
+string GetExceptionMessage(Exception e)
+{
+    return $"Exception message: {e.Message}";
 }
