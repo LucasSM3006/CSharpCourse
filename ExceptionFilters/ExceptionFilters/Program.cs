555730f4e8e0929e3e4765f6f9a8bc163c00ca39
LucasSM3006 <sasso3006@gmail.com>
2025-02-04 19:12:54 -0300

    Exception filters, 'when' keyword. Example on a http request.

--- ExceptionFilters/ExceptionFilters/Program.cs
+++ ExceptionFilters/ExceptionFilters/Program.cs
@@ -1,5 +1,30 @@
-﻿
+﻿using System.Linq.Expressions;
+
+try
+{
 var dataFromWeb = SendHttpRequest("https://www.joshua.com");
+}
+// catch (HttpRequestException ex) when (ex.Message.StartsWith("4")) // Commented out due to it catching all error 400's. This block is above, so it'll be the one to execute instead of the ones below.
+// {
+//    Console.WriteLine("Some kind of client error");
+//    throw;
+// }
+catch (HttpRequestException ex) when (ex.Message == "403")
+{
+    Console.WriteLine("Forbidden access to the resource");
+    throw;
+}
+catch (HttpRequestException ex) when (ex.Message == "404")
+{
+    Console.WriteLine("Resource not found");
+    throw;
+}
+catch (HttpRequestException ex) when (ex.Message == "500")
+{
+    Console.WriteLine("The server has experience an internal error");
+    throw;
+}
+
 
 Console.ReadKey();
 
