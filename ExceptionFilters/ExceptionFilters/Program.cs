ed5c559f5adf4bfe585503b9ad0116578c1db9ee
LucasSM3006 <sasso3006@gmail.com>
2025-02-04 19:07:05 -0300

    Filters for exceptions

---
+++ ExceptionFilters/ExceptionFilters/Program.cs
@@ -0,0 +1,16 @@
+﻿
+var dataFromWeb = SendHttpRequest("https://www.joshua.com");
+
+Console.ReadKey();
+
+
+
+string SendHttpRequest(string link)
+{
+    string response;
+
+    // implemnt
+    response = "boss";
+
+    return response;
+}
