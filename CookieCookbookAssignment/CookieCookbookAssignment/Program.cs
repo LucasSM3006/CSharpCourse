3fa6f8f642e28ec796df6d0c182f3fb389e22a02
LucasSM3006 <sasso3006@gmail.com>
2025-02-03 22:35:05 -0300

    Global try catch added to the entry point of the Cookie Cookbook application.

--- CookieCookbookAssignment/CookieCookbookAssignment/Program.cs
+++ CookieCookbookAssignment/CookieCookbookAssignment/Program.cs
@@ -4,19 +4,29 @@ using CookieCookbookAssignment.FileAccess;
 using CookieCookbookAssignment.Recipes;
 using CookieCookbookAssignment.Recipes.Ingredients;
 
-const FileType format = FileType.Json;
-const string fileName = "recipes";
+try
+{
+    const FileType format = FileType.Json;
+    const string fileName = "recipes";
 
-FileMetaData fileMetaData = new FileMetaData(fileName, format);
+    FileMetaData fileMetaData = new FileMetaData(fileName, format);
 
-IStringsRepository stringsRepository = format == FileType.Json ?
-    new StringsJsonRepository() :
-    new StringsTextualRepository();
+    IStringsRepository stringsRepository = format == FileType.Json ?
+        new StringsJsonRepository() :
+        new StringsTextualRepository();
 
-IngredientsRegister ingredientsRegister1 = new IngredientsRegister();
-RecipesConsoleUserInteraction console = new RecipesConsoleUserInteraction(ingredientsRegister1);
-TextRecipesRepository repository = new TextRecipesRepository(stringsRepository, ingredientsRegister1);
+    IngredientsRegister ingredientsRegister1 = new IngredientsRegister();
+    RecipesConsoleUserInteraction console = new RecipesConsoleUserInteraction(ingredientsRegister1);
+    TextRecipesRepository repository = new TextRecipesRepository(stringsRepository, ingredientsRegister1);
 
-CookiesRecipeApplication app = new CookiesRecipeApplication(repository, console);
+    CookiesRecipeApplication app = new CookiesRecipeApplication(repository, console);
 
-app.Run(fileMetaData.ToPath());
+    app.Run(fileMetaData.ToPath());
+}
+catch (Exception ex)
+{
+    Console.WriteLine("Unexpected error! The application will close.");
+    Console.WriteLine($"Error message: {ex.Message}");
+    Console.WriteLine("Press any key to close.");
+    Console.ReadKey();
+}
