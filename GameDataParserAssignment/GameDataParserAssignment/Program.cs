f902ecff18d902d68f1c8febb65893ce9b86f45e
LucasSM3006 <sasso3006@gmail.com>
2025-02-05 17:40:50 -0300

    Two interfaces for data manipulation & user interaction, along with the data class.

--- GameDataParserAssignment/GameDataParserAssignment/Program.cs
+++ GameDataParserAssignment/GameDataParserAssignment/Program.cs
@@ -1,17 +1,50 @@
 ﻿
 public class GameDataParserApplication
 {
+    private readonly IGameDataAppUserInteraction _gameUserInteraction;
+    private readonly IGameDataRepository _gameDataRepository;
+
+    public GameDataParserApplication(IGameDataAppUserInteraction gameDataAppUserInteraction, IGameDataRepository gameDataRepository)
+    {
+        _gameUserInteraction = gameDataAppUserInteraction;
+        _gameDataRepository = gameDataRepository;
+    }
+    
     public void Run()
     {
-        private readonly IGameDataAppUserInteraction _gameUserInteraction;
-        private readonly IGameDataRepository _gameDataRepository;
 
         _gameUserInteraction.ShowMessage("Enter the name of the file you want to read:");
-        JsonGameData gameData = _gameUserInteraction.PromptUserFileName();
 
-        _gameDataRepository.ReadDataFromJson();
+        string filename = _gameUserInteraction.PromptUserFileName();
+
+        _gameDataRepository.ReadDataFromJson(fileName);
 
         _gameUserInteraction.Exit();
     }
 }
 
+public interface IGameDataAppUserInteraction
+{
+    public void ShowMessage(string message);
+    public string PromptUserFileName();
+    public void Exit();
+}
+
+public interface IGameDataRepository
+{
+    public JsonGameData ReadDataFromJson(string fileName);
+}
+
+public class JsonGameData
+{
+    public string Title { get; }
+    public int ReleaseYear { get; }
+    public double Rating { get; }
+
+    public JsonGameData(string title, int releaseYear, double rating)
+    {
+        Title = title;
+        ReleaseYear = releaseYear;
+        Rating = rating;
+    }
+}
