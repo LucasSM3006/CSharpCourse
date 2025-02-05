7cd4d7a397263a18a693f07c8e175c21b5074b94
LucasSM3006 <sasso3006@gmail.com>
2025-02-04 21:26:55 -0300

    Example of a custom exception class.

--- ExceptionCustom/ExceptionCustom/Program.cs
+++ ExceptionCustom/ExceptionCustom/Program.cs
@@ -1,8 +1,42 @@
-﻿throw new CustomException();
+﻿using System.Runtime.Serialization;
+
+throw new Exception();
 
 Console.ReadKey();
 
+// [Serializable]
 public class CustomException : Exception
 {
+    public int StatusCode { get; }
 
+    // These are additionals. Microsoft recommends only the 'Exception' ending and three constructors. But it's useful to know these things exist.
+    //protected CustomException(SerializationInfo info, StreamingContext context) : base(info, context)
+    //{
+
+    //}
+
+    public CustomException()
+    {
+        
+    }
+
+    public CustomException(string message, int statusCode) : base(message)
+    {
+        StatusCode = statusCode;
+    }
+    
+    public CustomException(string message, int statusCode, Exception innerException) : base(message)
+    {
+        StatusCode = statusCode;
+    }
+
+    public CustomException(string message) : base(message)
+    {
+        
+    }
+
+    public CustomException(string message, Exception innerException) : base(message, innerException)
+    {
+        
+    }
 }
