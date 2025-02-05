204a8d802d76909d23e2da6e541c50c550cfc003
LucasSM3006 <sasso3006@gmail.com>
2025-02-04 21:40:06 -0300

    Custom exceptions made, JsonParsingException has a custom constructor due to us needing to display the problematic JSON in question.

--- ExceptionsWhenToUseCustomTypes/ExceptionsWhenToUseCustomTypes/Program.cs
+++ ExceptionsWhenToUseCustomTypes/ExceptionsWhenToUseCustomTypes/Program.cs
@@ -1,25 +1,101 @@
-﻿try
+﻿using System.Runtime.Serialization;
+
+try
 {
     ComplexMethod();
 }
-catch (InvalidOperationException ex) when (ex.Message == "Cannot connect to a service")
+catch (ConnectionException ex)
 {
     Console.WriteLine("Check your internet connection");
     throw;
 }
+catch (JsonParsingException ex)
+{
+    Console.WriteLine($"Unable to parse JSON. Json body is: {ex.JsonBody}");
+    throw;
+}
+
 Console.ReadKey();
 
 void ComplexMethod()
 {
     // step 1: connecting
-    throw new InvalidOperationException("Cannot connect to a service");
+    throw new ConnectionException("Cannot connect to a service");
 
     // step 2: authorizing
-    throw new InvalidOperationException("Couldn't authorize user");
+    throw new AuthorizationException("Couldn't authorize user");
 
     // step 3: retrieving data as JSON
-    throw new InvalidOperationException("Cannot retrieve data");
+    throw new DataAccessException("Cannot retrieve data");
 
     // step 4: parsing JSON to some C# type
-    throw new InvalidOperationException("Could not parse correctly");
+    throw new JsonParsingException("Could not parse correctly");
+}
+
+
+internal class JsonParsingException : Exception
+{
+    public string JsonBody { get; }
+    public JsonParsingException()
+    {
+    }
+
+    public JsonParsingException(string? message) : base(message)
+    {
+    }
+    
+    public JsonParsingException(string? message, string jsonBody) : base(message)
+    {
+        JsonBody = jsonBody;
+    }
+
+    public JsonParsingException(string? message, Exception? innerException, string jsonBody) : base(message, innerException)
+    {
+        JsonBody = jsonBody;
+    }
+}
+
+internal class DataAccessException : Exception
+{
+    public DataAccessException()
+    {
+    }
+
+    public DataAccessException(string? message) : base(message)
+    {
+    }
+
+    public DataAccessException(string? message, Exception? innerException) : base(message, innerException)
+    {
+    }
+}
+
+internal class AuthorizationException : Exception
+{
+    public AuthorizationException()
+    {
+    }
+
+    public AuthorizationException(string? message) : base(message)
+    {
+    }
+
+    public AuthorizationException(string? message, Exception? innerException) : base(message, innerException)
+    {
+    }
+}
+
+internal class ConnectionException : Exception
+{
+    public ConnectionException()
+    {
+    }
+
+    public ConnectionException(string? message) : base(message)
+    {
+    }
+
+    public ConnectionException(string? message, Exception? innerException) : base(message, innerException)
+    {
+    }
 }
