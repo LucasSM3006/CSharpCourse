ab5a40e97798562bc2bdc7bf66b7b805d345293a
LucasSM3006 <sasso3006@gmail.com>
2025-02-04 21:33:38 -0300

    When to use custom exceptions.

---
+++ ExceptionsWhenToUseCustomTypes/ExceptionsWhenToUseCustomTypes/Program.cs
@@ -0,0 +1,25 @@
+﻿try
+{
+    ComplexMethod();
+}
+catch (InvalidOperationException ex) when (ex.Message == "Cannot connect to a service")
+{
+    Console.WriteLine("Check your internet connection");
+    throw;
+}
+Console.ReadKey();
+
+void ComplexMethod()
+{
+    // step 1: connecting
+    throw new InvalidOperationException("Cannot connect to a service");
+
+    // step 2: authorizing
+    throw new InvalidOperationException("Couldn't authorize user");
+
+    // step 3: retrieving data as JSON
+    throw new InvalidOperationException("Cannot retrieve data");
+
+    // step 4: parsing JSON to some C# type
+    throw new InvalidOperationException("Could not parse correctly");
+}
