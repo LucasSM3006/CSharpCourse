3534b5c11f98d255970dfb49cac2cd479226e670
LucasSM3006 <sasso3006@gmail.com>
2025-02-06 19:50:31 -0300

    Committing and pushing this in case there's a power outage. Thunder's striking right now.

--- GenericTypes/GenericTypes/Program.cs
+++ GenericTypes/GenericTypes/Program.cs
@@ -1,3 +1,23 @@
 ﻿List<int> numbers = new List<int> { 1, 3, 1, 2, 41 };
 List<string> words = new List<string> { "aaa", "bbb" };
 List<DateTime> dates = new List<DateTime> { DateTime.Now, new DateTime(2025, 5, 30) };
+
+
+public class IntList
+{
+    private int[] _items = new int[4];
+    private int _size = 0;
+
+    public void Add(int item)
+    {
+        if(_size >= _items.Length)
+        {
+            int[] newItems = new int[_items.Length * 2];
+
+            for(int i = 0; i < )
+        }
+        _items[_size] = item;
+        ++_size;
+    }
+
+}
