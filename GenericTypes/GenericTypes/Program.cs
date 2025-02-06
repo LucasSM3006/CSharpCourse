5a1b67430988cbd024379e06ae17ad2fc52fa316
LucasSM3006 <sasso3006@gmail.com>
2025-02-06 19:30:27 -0300

    Generic types, first example, list

---
+++ GenericTypes/GenericTypes/Program.cs
@@ -0,0 +1,3 @@
+﻿List<int> numbers = new List<int> { 1, 3, 1, 2, 41 };
+List<string> words = new List<string> { "aaa", "bbb" };
+List<DateTime> dates = new List<DateTime> { DateTime.Now, new DateTime(2025, 5, 30) };
