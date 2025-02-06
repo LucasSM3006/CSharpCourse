21234674cfa0e5a85da918ae9f5905035d17d9bd
LucasSM3006 <sasso3006@gmail.com>
2025-02-05 22:29:43 -0300

    Holds all files related to logging.

---
+++ GameDataParserAssignment/GameDataParserAssignment/Logging/Logger.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameDataParserAssignment.Logging
+{
+    public class Logger
+    {
+        private readonly string LogFileName;
+
+        public Logger(string logFileName)
+        {
+            LogFileName = logFileName;
+        }
+
+        public void Log(Exception ex)
+        {
+            string entry =
+    $@"[{DateTime.Now}]
+Exception message: {ex.Message}
+Stack trace: {ex.StackTrace}
+
+";
+
+            File.AppendAllText(LogFileName, entry); // creates a log if it doesn't exist.
+        }
+    }
+
+}
