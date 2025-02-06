d572bdb641813088639aec124bd6d396416877d0
LucasSM3006 <sasso3006@gmail.com>
2025-02-05 22:30:07 -0300

    Holds the classes related to data

---
+++ GameDataParserAssignment/GameDataParserAssignment/Model/GameData.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameDataParserAssignment.Model
+{
+    public class GameData
+    {
+        public string Title { get; }
+        public int ReleaseYear { get; }
+        public double Rating { get; }
+
+        public GameData(string title, int releaseYear, double rating)
+        {
+            Title = title;
+            ReleaseYear = releaseYear;
+            Rating = rating;
+        }
+
+        public override string ToString()
+        {
+            return $"{Title}, released in {ReleaseYear}, with a rating of {Rating}";
+        }
+    }
+}
