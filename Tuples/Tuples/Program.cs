35bc9b16f14fc086827760c521c32f81845da497
LucasSM3006 <sasso3006@gmail.com>
2025-02-06 22:56:08 -0300

    Changed the type from SimpleTuple to the built-in Tuple type.

--- Tuples/Tuples/Program.cs
+++ Tuples/Tuples/Program.cs
@@ -1,12 +1,15 @@
 ﻿List<int> numbers = new List<int> { 5, 3, 2, 8, 16, 7 };
-SimpleTuple<int, int> minAndMax = GetMinAndMax(numbers);
-Console.WriteLine($"Smallest number is {minAndMax.Element1}");
-Console.WriteLine($"Biggest number is {minAndMax.Element2}");
-SimpleTuple<int, int, int> threeitems; // Different type of SimpleTuple, just shares the name.
+/*Simple*/Tuple<int, int> minAndMax = GetMinAndMax(numbers);
+/*Simple*/Tuple<string, int> stringAndInt = new Tuple<string, int> ("aaa", 2);
+
+Console.WriteLine($"Smallest number is {minAndMax.Item1}");
+Console.WriteLine($"Biggest number is {minAndMax.Item2}");
+
+/*Simple*/Tuple<int, int, int> threeitems; // Different type of SimpleTuple, just shares the name.
 
 Console.ReadKey();
 
-SimpleTuple<int, int> GetMinAndMax(IEnumerable<int> input)
+/*Simple*/Tuple<int, int> GetMinAndMax(IEnumerable<int> input)
 {
     if(!input.Any())
     {
@@ -28,31 +31,31 @@ SimpleTuple<int, int> GetMinAndMax(IEnumerable<int> input)
         }
     }
 
-    return new SimpleTuple <int, int> (min, max);
+    return new /*Simple*/Tuple <int, int> (min, max);
 }
 
 public class SimpleTuple<T1, T2>
 {
-    public SimpleTuple(T1 element1, T2 element2)
+    public SimpleTuple(T1 item1, T2 item2)
     {
-        Element1 = element1;
-        Element2 = element2;
+        Item1 = item1;
+        Item2 = item2;
     }
 
-    public T1 Element1 { get; }
-    public T2 Element2 { get; }
+    public T1 Item1 { get; }
+    public T2 Item2 { get; }
 }
 
 public class SimpleTuple<T1, T2, T3>
 {
-    public SimpleTuple(T1 element1, T2 element2, T3 element3)
+    public SimpleTuple(T1 item1, T2 item2, T3 item3)
     {
-        Element1 = element1;
-        Element2 = element2;
-        Element3 = element3;
+        Item1 = item1;
+        Item2 = item2;
+        Item3 = item3;
     }
 
-    public T1 Element1 { get; }
-    public T2 Element2 { get; }
-    public T3 Element3 { get; }
+    public T1 Item1 { get; }
+    public T2 Item2 { get; }
+    public T3 Item3 { get; }
 }
