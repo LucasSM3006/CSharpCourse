f60aaa40ff54656eb2de728e44fbdd771ae448f8
LucasSM3006 <sasso3006@gmail.com>
2025-02-07 19:50:23 -0300

    Generic methods, and a throwback to extensions. They can run some action on multiple different types.

---
+++ GenericMethods/GenericMethods/Program.cs
@@ -0,0 +1,20 @@
+﻿
+List<int> ints = new List<int> { 1, 2, 3 };
+
+Console.WriteLine(ints[0]);
+
+ints.AddToFront(10);
+// ints.AddToFront<int>(10); // This wasn't needed due to the type being inferred by the compiler. 
+// ints.AddToFront("abc"); // Generates a compiling error, 'The type arguments for method AddToFront cannot be inferred from the usage.'  
+
+Console.WriteLine(ints[0]);
+
+Console.ReadKey();
+
+static class ListExtensions
+{
+    public static void AddToFront<T>(this List<T> list, T item)
+    {
+        list.Insert(0, item);
+    }
+}
