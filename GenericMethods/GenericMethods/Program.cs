df65ad2b9d4eaf24b04bf36f6c97b577568dbd75
LucasSM3006 <sasso3006@gmail.com>
2025-02-07 20:47:09 -0300

    Generic method with multiple parameters

--- GenericMethods/GenericMethods/Program.cs
+++ GenericMethods/GenericMethods/Program.cs
@@ -9,6 +9,21 @@ ints.AddToFront(10);
 
 Console.WriteLine(ints[0]);
 
+List<decimal> decimals = new List<decimal> { 11.2m, 22.4m, 22.3m, 0.5m };
+
+foreach (decimal d in decimals)
+{
+    Console.WriteLine(d);
+}
+
+// List<int> integers = (List<int>)decimals; // This doesn't work, we need to cast each individual item.
+List<int> integers = decimals.ConvertTo<decimal, int>();
+
+foreach (int i in integers)
+{
+    Console.WriteLine(i);
+}
+
 Console.ReadKey();
 
 static class ListExtensions
@@ -17,4 +32,19 @@ static class ListExtensions
     {
         list.Insert(0, item);
     }
+
+    // Generic method with multiple parameters.
+    public static List<TTarget> ConvertTo<TSource, TTarget>(this List<TSource> input)
+    {
+        var result = new List<TTarget>();
+
+        foreach (var item in input)
+        {
+            TTarget itemAfterCasting = (TTarget) Convert.ChangeType(item, typeof(TTarget)); // Must be cast to the type we want due to it returning a System object.
+            result.Add(itemAfterCasting);
+        }
+
+        return result;
+    }
 }
+
