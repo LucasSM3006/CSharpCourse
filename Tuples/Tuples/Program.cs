e2e56c1b0cc8b43202e8a150ce0ab3ae6d0a47b1
LucasSM3006 <sasso3006@gmail.com>
2025-02-06 22:52:30 -0300

    Example of one tuple with three class.

--- Tuples/Tuples/Program.cs
+++ Tuples/Tuples/Program.cs
@@ -2,6 +2,7 @@
 SimpleTuple<int, int> minAndMax = GetMinAndMax(numbers);
 Console.WriteLine($"Smallest number is {minAndMax.Element1}");
 Console.WriteLine($"Biggest number is {minAndMax.Element2}");
+SimpleTuple<int, int, int> threeitems; // Different type of SimpleTuple, just shares the name.
 
 Console.ReadKey();
 
@@ -42,3 +43,16 @@ public class SimpleTuple<T1, T2>
     public T2 Element2 { get; }
 }
 
+public class SimpleTuple<T1, T2, T3>
+{
+    public SimpleTuple(T1 element1, T2 element2, T3 element3)
+    {
+        Element1 = element1;
+        Element2 = element2;
+        Element3 = element3;
+    }
+
+    public T1 Element1 { get; }
+    public T2 Element2 { get; }
+    public T3 Element3 { get; }
+}
