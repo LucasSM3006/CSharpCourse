c5ccff4d8f7c71d107184594a3bb33f1c7c92ad3
LucasSM3006 <sasso3006@gmail.com>
2025-02-08 19:09:30 -0300

    Sorting lists. We can use the Sort() method to sort lists holding simple types, and will be seeing an example of a list of a more complex type.

---
+++ IComparable/IComparable/Program.cs
@@ -0,0 +1,24 @@
+﻿
+List<Person> people = new List<Person> { 
+    new Person { Name = "Jane", YearOfBirth = 1999 },
+    new Person { Name = "John", YearOfBirth = 2001 },
+    new Person { Name = "Joanne", YearOfBirth = 2000 }
+};
+
+foreach (Person person in people)
+{
+    Console.WriteLine(person.Name);
+}
+
+people.Sort(); // Throws an exception. Only works if the list implements the IComparable interface.
+
+foreach (Person person in people)
+{
+    Console.WriteLine(person.Name);
+}
+
+public class Person
+{
+    public string Name { get; init; }
+    public int YearOfBirth { get; init; }
+}
