accef04dccc2c4e677a12deb6689bef9000b5d36
LucasSM3006 <sasso3006@gmail.com>
2025-02-08 20:42:55 -0300

    Methods can have multiple generic types and also multiple constraints

---
+++ GenericTypeConstraintsMultipleTypes/GenericTypeConstraintsMultipleTypes/Program.cs
@@ -0,0 +1,10 @@
+﻿
+Console.ReadKey();
+
+void SomeMethod<TPet, TOwner>(TPet pet, TOwner owner) where TPet: Pet, IComparable<TPet> where TOwner : new()
+{
+
+}
+
+public class Pet { }
+public class PetOwner { }
