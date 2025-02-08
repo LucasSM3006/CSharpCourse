497eed57cd2001f90f6d7b517a42a648d5dac1e9
LucasSM3006 <sasso3006@gmail.com>
2025-02-08 20:31:31 -0300

    Before .NET 7, released in 2022, you had to do it like that to hhandle multiple types.

---
+++ GenericConstraintsForNumbers/GenericConstraintsForNumbers/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine($"Square of 2 is: {Calculator.Square(2)}");
+Console.WriteLine($"Square of 4m is: {Calculator.Square(4m)}");
+Console.WriteLine($"Square of 6d is: {Calculator.Square(6d)}");
+
+
+Console.ReadKey();
+
+public static class Calculator
+{
+    public static int Square(int input) => input * input;
+    public static decimal Square(decimal input) => input * input;
+    public static double Square(double input) => input * input;
+}
