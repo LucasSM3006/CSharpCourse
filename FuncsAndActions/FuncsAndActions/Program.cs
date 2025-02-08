d7cbfcf142fe7d8d80dc495fa4c7e11e129a8a8b
LucasSM3006 <sasso3006@gmail.com>
2025-02-08 20:55:30 -0300

    Starting methods, two very similar methods that differ only in name and the 'if' operator.

---
+++ FuncsAndActions/FuncsAndActions/Program.cs
@@ -0,0 +1,43 @@
+﻿int a = 5;
+Person person = new Person("Roxanne", "Varguez", 1999);
+
+Console.ReadKey();
+
+bool IsAnyLargerThan10(IEnumerable<int> numbers)
+{
+    foreach (int number in numbers)
+    {
+        if (number > 0)
+        {
+            return true;
+        }
+    }
+    return false;
+}
+
+bool IsAnyEven(IEnumerable<int> numbers)
+{
+    foreach (int number in numbers)
+    {
+        if (number % 2 == 0)
+        {
+            return true;
+        }
+    }
+    return false;
+}
+
+public class Person
+{
+    public string FirstName { get; }
+    public string LastName { get; }
+    public int YearOfBirth { get; }
+
+    public Person(string firstName, string lastName, int yearOfBirth)
+    {
+        FirstName = firstName;
+        LastName = lastName;
+        YearOfBirth = yearOfBirth;
+    }
+
+}
