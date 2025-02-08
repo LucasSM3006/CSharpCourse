67e997f946f805e9b66a3d9c8c963ed8adad488d
LucasSM3006 <sasso3006@gmail.com>
2025-02-07 22:03:30 -0300

    Example of how to limit a method to classes derived from a specific type.

--- GenericConstraints/GenericConstraints/Program.cs
+++ GenericConstraints/GenericConstraints/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System.Diagnostics;
+using System.Threading.Channels;
 
 Stopwatch sw = Stopwatch.StartNew();
 
@@ -6,6 +7,27 @@ var list = CreateCollectionOfRandomLength<int>(100);
 
 sw.Stop();
 Console.WriteLine($"number of miliseconds: {sw.ElapsedMilliseconds}");
+
+var people = new List<Person>
+{ new Person { Name = "dfaaf", YearOfBirth = 1999 },
+  new Person { Name = "fafa", YearOfBirth = 2002 },
+  new Person { Name = "fafa22", YearOfBirth = 2000 }
+};
+
+var employees = new List<Employee>
+{ new Employee { Name = "daf", YearOfBirth = 1999 },
+  new Employee { Name = "aa", YearOfBirth = 20224 },
+  new Employee { Name = "fa22", YearOfBirth = 2029 }
+};
+
+var validPeople = GetOnlyValid(people);
+var validEmployee = GetOnlyValid(employees);
+
+foreach (var employee in validEmployee)
+{
+    employee.GoToWork();
+}
+
 Console.ReadKey();
 
 IEnumerable<T> CreateCollectionOfRandomLength<T>(int maxLength) where T : new() // Gaurantees there's an empty constructor that takes no parameters.
@@ -20,4 +42,31 @@ IEnumerable<T> CreateCollectionOfRandomLength<T>(int maxLength) where T : new() 
     }
 
     return collection;
+}
+
+IEnumerable<TPerson> GetOnlyValid<TPerson>(IEnumerable<TPerson> people) where TPerson : Person // Again using 'where'. This forces it to only accept classes that derive from Person
+{
+    var result = new List<TPerson>();
+
+    foreach(TPerson person in people)
+    {
+        if(person.YearOfBirth > 1900 && person.YearOfBirth < DateTime.Now.Year)
+        {
+            result.Add(person); 
+        }
+    }
+
+    return result;
+}
+
+
+public class Person
+{
+    public string Name { get; init; }
+    public int YearOfBirth { get; init; }
+}
+
+public class Employee : Person
+{
+    public void GoToWork() => Console.WriteLine("I have to go to work!");
 }
