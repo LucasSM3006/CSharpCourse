119eba14cc73d47613a193238ae89f5e2b84c544
LucasSM3006 <sasso3006@gmail.com>
2025-02-10 20:52:17 -0300

    Lambda expressions, more ways to use them.

---
+++ Exercise35/Exercise35/Program.cs
@@ -0,0 +1,20 @@
+﻿/*
+ * 
+ * Basics of lambda expressions
+
+The Exercise class has two fields of Func type. To each of them, assign a lambda expression so that:
+
+    GetLength stores a function taking a string and returning its length.
+
+    GetRandomNumberBetween1And10 stores a parameterless function generating a random number between 1 and 10.
+ * 
+ * 
+ */
+
+
+
+public class Exercise
+{
+    public Func<string, int> GetLength = // your code goes here;
+        public Func<int> GetRandomNumberBetween1And10 = // your code goes here;
+    }
