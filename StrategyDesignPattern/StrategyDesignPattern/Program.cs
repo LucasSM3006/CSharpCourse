afc9ba75628d57416daf32e8634fda999d69ad56
LucasSM3006 <sasso3006@gmail.com>
2025-02-11 19:55:45 -0300

    Moved it into a proper class

--- StrategyDesignPattern/StrategyDesignPattern/Program.cs
+++ StrategyDesignPattern/StrategyDesignPattern/Program.cs
@@ -8,85 +8,90 @@ Odd");
 
 var userInput = Console.ReadLine();
 
-List<int> result;
-switch(userInput.ToLower())
-{
-    case "even":
-        result = SelectEven(numbers);
-        break;
-    case "odd":
-        result = SelectOdd(numbers);
-        break;
-    case "positive":
-        result = SelectPositive(numbers);
-        break;
-    case "negative":
-        result = SelectNegative(numbers);
-        break;
-    default:
-        throw new NotSupportedException($"Invalid user input, {userInput} is not a filter.");
-}
+List<int> result = new NumbersFilter().FilterBy(userInput, numbers);
 
 Print(result);
 Print(numbers);
 
 Console.ReadKey();
 
-List<int> SelectEven(List<int> numbers)
-{
-    List<int> evenNumbers = new List<int>();
-
-    foreach (int number in numbers)
-    {
-        if(number % 2 == 0) evenNumbers.Add(number);
-    }
-
-    return evenNumbers;
-}
-
-List<int> SelectOdd(List<int> numbers)
-{
-    List<int> oddNumbers = new List<int>();
-
-    foreach (int number in numbers)
-    {
-        if (number % 2 != 0) oddNumbers.Add(number);
-    }
-
-    return oddNumbers;
-}
-
-List<int> SelectPositive(List<int> numbers)
-{
-    List<int> positiveNumbers = new List<int>();
-
-    foreach(int number in numbers)
-    {
-        if (number > 0)
-        {
-            positiveNumbers.Add(number);
-        }
-    }
-
-    return positiveNumbers;
-}
-
-List<int> SelectNegative(List<int> numbers)
-{
-    List<int> negativeNumbers = new List<int>();
-
-    foreach (int number in numbers)
-    {
-        if (number < 0)
-        {
-            negativeNumbers.Add(number);
-        }
-    }
-
-    return negativeNumbers;
-}
 
 void Print(IEnumerable<int> numbers)
 {
     Console.WriteLine(string.Join(", ", numbers));
+}
+
+public class NumbersFilter
+{
+    public List<int> FilterBy(string filteringType, List<int> numbers)
+    {
+        switch (filteringType.ToLower())
+        {
+            case "even":
+                return SelectEven(numbers);
+            case "odd":
+                return SelectOdd(numbers);
+            case "positive":
+                return SelectPositive(numbers);
+            case "negative":
+                return SelectNegative(numbers);
+            default:
+                throw new NotSupportedException($"Invalid user input, {filteringType} is not a filter.");
+        }
+    }
+
+    private List<int> SelectEven(List<int> numbers)
+    {
+        List<int> evenNumbers = new List<int>();
+
+        foreach (int number in numbers)
+        {
+            if (number % 2 == 0) evenNumbers.Add(number);
+        }
+
+        return evenNumbers;
+    }
+
+    private List<int> SelectOdd(List<int> numbers)
+    {
+        List<int> oddNumbers = new List<int>();
+
+        foreach (int number in numbers)
+        {
+            if (number % 2 != 0) oddNumbers.Add(number);
+        }
+
+        return oddNumbers;
+    }
+
+    private List<int> SelectPositive(List<int> numbers)
+    {
+        List<int> positiveNumbers = new List<int>();
+
+        foreach (int number in numbers)
+        {
+            if (number > 0)
+            {
+                positiveNumbers.Add(number);
+            }
+        }
+
+        return positiveNumbers;
+    }
+
+    private List<int> SelectNegative(List<int> numbers)
+    {
+        List<int> negativeNumbers = new List<int>();
+
+        foreach (int number in numbers)
+        {
+            if (number < 0)
+            {
+                negativeNumbers.Add(number);
+            }
+        }
+
+        return negativeNumbers;
+    }
+
 }
