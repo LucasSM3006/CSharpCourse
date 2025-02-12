002381675b50dc9180c9718a89fe98063e5b6011
LucasSM3006 <sasso3006@gmail.com>
2025-02-12 20:32:27 -0300

    Changes to the code, more following the course now

--- CustomCacheAssignment/CustomCacheAssignment/Program.cs
+++ CustomCacheAssignment/CustomCacheAssignment/Program.cs
@@ -8,6 +8,8 @@ Console.WriteLine(dataDownloader.DownloadData("id3"));
 Console.WriteLine(dataDownloader.DownloadData("id1"));
 Console.WriteLine(dataDownloader.DownloadData("id2"));
 
+Console.ReadKey();
+
 public interface IDataDownloader
 {
     public string DownloadData(string id);
@@ -15,14 +17,18 @@ public interface IDataDownloader
 
 public class SlowDataDownloader : IDataDownloader
 {
+
+    private readonly Cache<string, string> _cache = new();
+
     public string DownloadData(string id)
     {
-        if(CachedData.FindById(id) is not null)
-        {
-            return $"some data for {id}";
-        }
+        return _cache.Get(id, DownloadDataWithoutCaching);
+    }
 
+    private string DownloadDataWithoutCaching(string id)
+    {
         // Slow data grabbing and downloading
+
         Thread.Sleep(1000);
         return $"some data for {id}";
     }
@@ -31,51 +37,19 @@ public class SlowDataDownloader : IDataDownloader
 public class Cache<TKey, TData>
 {
     private Dictionary<TKey, TData> _cachedData = new Dictionary<TKey, TData>();
-    private IDataDownloader _dataDownloader;
-
-    public Cache(IDataDownloader dataDownloader)
-    {
-        _dataDownloader = dataDownloader;
-    }
 
     private void StoreData<TData>(TData data)
     {
 
     }
 
-    private TData Get(TKey key)
+    public TData Get(TKey key, Func<TKey, TData> getForTheFirstTime) // key identifies some data, getFor... uses the key to retrieve data.
     {
         if (!_cachedData.ContainsKey(key))
         {
-            _cachedData[key] = _dataDownloader.DownloadData(key);
+            _cachedData[key] = getForTheFirstTime(key);
         }
 
         return _cachedData[key];
-    }
-}
-
-public interface IDataRepository<TKey, TData>
-{
-    public void Save(TKey key, TData data);
-    public KeyValuePair<TKey, TData> GetById(TKey key);
-}
-
-public class DataRepository<TKey, TData> : IDataRepository<TKey, TData>
-{
-    private Dictionary<TKey, TData> data = new Dictionary<TKey, TData> ();
-
-    public DataRepository()
-    {
-        data.Add(new KeyValuePair<TKey, TData>(TKey, TData));
-    }
-
-    public KeyValuePair<TKey, TData> GetById(TKey key)
-    {
-        throw new NotImplementedException();
-    }
-
-    public void Save(TKey key, TData data)
-    {
-        throw new NotImplementedException();
     }
 }
