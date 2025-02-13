0cc70921c6320a55870731ce6328a59d2ab34a0a
LucasSM3006 <sasso3006@gmail.com>
2025-02-13 20:43:51 -0300

    Use of the 'Any' method, which allows us to find if one element matches a certain condition.

--- LINQ3/LINQ3/Program.cs
+++ LINQ3/LINQ3/Program.cs
@@ -23,4 +23,32 @@ Console.WriteLine($"Odd numbers: {{ {string.Join(", ", oddNumbers)} }}");
 Console.WriteLine($"Ordered numbers: {{ {string.Join(", ", orderedOddNumbers)} }}");
 Console.WriteLine($"Ordered numbers ALT: {{ {string.Join(", ", orderedOddNumbersALT)} }}");
 
+Console.WriteLine("Hello!");
+
+var randomWords = new List<string> { "aaaaa", "bbasdfas", "buffon", "verylongword", "o"};
+
+if(randomWords.Any(word => word.Length == 1))
+{
+    Console.WriteLine("There's one word in the list with a length of one.");
+}
+
+if(randomWords.Any(word => word.StartsWith('a')))
+{
+    Console.WriteLine("There's one word in the list that starts with 'a'.");
+}
+
+var randomNumbers = new[] { 5, 9, 8, 12, 200 };
+
+if(randomNumbers.Any(number => number > 10))
+{
+    Console.WriteLine("There's a number larger than 10!");
+}
+
+bool isAnyLargerThan100 = randomNumbers.Any(number => number > 100);
+
+if(isAnyLargerThan100)
+{
+    Console.WriteLine("There's a number larger than 100.");
+}
+
 Console.ReadKey();
