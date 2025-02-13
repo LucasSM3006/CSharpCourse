78c699b48c665d4d8de892e1a041f838224a2da9
LucasSM3006 <sasso3006@gmail.com>
2025-02-13 19:01:02 -0300

    More LINQ examples, this time we use Append. There's more methods, such as Where, which return a value, but don't modify the input.

---
+++ LINQ3/LINQ3/Program.cs
@@ -0,0 +1,11 @@
+﻿// Note: Linq does not modify the input, it instead creates a new one.
+
+var numbers = new List<int> { 5, 3, 7, 1, 2, 4 };
+var numbersWith10 = numbers.Append(10);
+
+// Despite appending ten to it on the line above, the input, 'numbers' remained unmodified.
+
+Console.WriteLine($"Numbers without appending 10: {{ {string.Join(", ", numbers)} }}");
+Console.WriteLine($"Numbers with appending 10: {{ {string.Join(", ", numbersWith10)} }}");
+
+Console.ReadKey();
