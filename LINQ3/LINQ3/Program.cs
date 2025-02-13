f210b5606056b64c609813a9a413c7ec078e782b
LucasSM3006 <sasso3006@gmail.com>
2025-02-13 19:15:53 -0300

    Example of method chaining.

--- LINQ3/LINQ3/Program.cs
+++ LINQ3/LINQ3/Program.cs
@@ -8,4 +8,19 @@ var numbersWith10 = numbers.Append(10);
 Console.WriteLine($"Numbers without appending 10: {{ {string.Join(", ", numbers)} }}");
 Console.WriteLine($"Numbers with appending 10: {{ {string.Join(", ", numbersWith10)} }}");
 
+// Another example;
+
+var oddNumbers = numbers.Where(number => number % 2 != 0);
+var orderedOddNumbers = oddNumbers.OrderBy(number => number);
+
+// It's very similar to streams in Java, but much simpler..
+var orderedOddNumbersALT = numbers
+    .Where(number => number % 2 != 0)
+    .OrderBy(number => number);
+// Code above is method chaining.
+
+Console.WriteLine($"Odd numbers: {{ {string.Join(", ", oddNumbers)} }}");
+Console.WriteLine($"Ordered numbers: {{ {string.Join(", ", orderedOddNumbers)} }}");
+Console.WriteLine($"Ordered numbers ALT: {{ {string.Join(", ", orderedOddNumbersALT)} }}");
+
 Console.ReadKey();
