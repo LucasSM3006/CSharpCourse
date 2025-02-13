ec5f64dbff0ffe13a021a737543f8b84f8ebd6e2
LucasSM3006 <sasso3006@gmail.com>
2025-02-12 22:09:03 -0300

    Project to explore the LINQ library, starting by making a LINQ code that is already implemented by LINQ to demonstrate how it'd be if we didn't use LINQ.

---
+++ LINQ/LINQ/Program.cs
@@ -0,0 +1,31 @@
+﻿// LINQ is a set of technologies that allow simple and efficient querying over different kinds of data.
+// Language Integrated Query
+// Allows filtering, ordering, and transforming the collection elements, and more.
+
+List<string> words = new List<string> { "BalLs", "LsL", "Dee", "heeasd", "BEATS" };
+
+Console.WriteLine(IsAnyWordUpperCase(words));
+
+Console.ReadKey();
+
+static bool IsAnyWordUpperCase(IEnumerable<string> words)
+{
+    foreach (string word in words)
+    {
+        List<char> characters = word.ToList();
+        bool isUpper = false;
+
+        foreach (char c in characters)
+        {
+            if (char.IsUpper(c))
+            {
+                isUpper = true;
+                continue;
+            }
+            isUpper = false;
+            break;
+        }
+        if (isUpper) return true;
+    }
+    return false;
+}
