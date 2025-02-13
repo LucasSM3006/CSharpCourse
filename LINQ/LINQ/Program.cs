bf70ae4294f2c8c0212d3461f62a34280521f5e6
LucasSM3006 <sasso3006@gmail.com>
2025-02-12 22:17:12 -0300

    First LINQ query

--- LINQ/LINQ/Program.cs
+++ LINQ/LINQ/Program.cs
@@ -2,7 +2,7 @@
 // Language Integrated Query
 // Allows filtering, ordering, and transforming the collection elements, and more.
 
-List<string> words = new List<string> { "BalLs", "LsL", "Dee", "heeasd", "BEATS" };
+List<string> words = new List<string> { "BalLs", "LsL", "Dee", "heeasd", "BEATsS" };
 
 Console.WriteLine(IsAnyWordUpperCase(words));
 
@@ -13,19 +13,24 @@ static bool IsAnyWordUpperCase(IEnumerable<string> words)
     foreach (string word in words)
     {
         List<char> characters = word.ToList();
-        bool isUpper = false;
+        bool isUpper = true;
 
         foreach (char c in characters)
         {
-            if (char.IsUpper(c))
+            if (char.IsLower(c))
             {
-                isUpper = true;
-                continue;
+                isUpper = false;
+                break;
             }
-            isUpper = false;
-            break;
         }
         if (isUpper) return true;
     }
     return false;
+}
+
+static bool IsAnyWordUpperCaseLINQ(IEnumerable<string> words)
+{
+    return words.Any(word => word.All(letter => char.IsUpper(letter)));
+    // STREAM, LITERALLY A STREAM.
+    // Okay, so, words has words, we use Any to iterate through, and word is the element from the list. "All" will determine if all elements satisfy a condition, the condition? ARE THEY UPPER? It's a generic method using a Func, just like we've seen before!
 }
