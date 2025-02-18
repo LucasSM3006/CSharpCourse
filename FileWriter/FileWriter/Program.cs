ffc616cc0bcc659eb7a83958ebfdb1c8693f366d
LucasSM3006 <sasso3006@gmail.com>
2025-02-18 20:02:02 -0300

    The purpose of this is to demonstrate the disposable method. At the moment, it doesn't write to the file, but it does create it. The thing about it, however, is that it seemingly doesn't allow you to mess with the file while the program is running, and that is because it's open in the app.

---
+++ FileWriter/FileWriter/Program.cs
@@ -0,0 +1,22 @@
+﻿const string filePath = "file.txt";
+var writer = new FileWriter(filePath);
+
+writer.Write("Testing this with a fairly larger string that is full of words");
+writer.Write("This should be in the second line of the text file due to the environment.newline we set earlier");
+
+Console.WriteLine("Press any key to close.");
+Console.ReadKey();
+
+public class FileWriter
+{
+    private readonly StreamWriter _writer;
+    public FileWriter(string filePath)
+    {
+        _writer = new StreamWriter(filePath, true); // "true" is to append it.
+    }
+
+    public void Write(string text)
+    {
+        _writer.WriteLine(text + Environment.NewLine);
+    }
+}
