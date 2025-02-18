731a4363decc8e0c20a9c945207370c3d42c49fa
LucasSM3006 <sasso3006@gmail.com>
2025-02-18 20:43:50 -0300

    'using', disposing of the variables like a try/finally.

--- FileWriter/FileWriter/Program.cs
+++ FileWriter/FileWriter/Program.cs
@@ -1,19 +1,22 @@
 ﻿const string filePath = "file.txt";
-var writer = new FileWriter(filePath); // Commented out to stop text from being written.
-// var reader = new SpecificLineFromTextFileReader(filePath);
 
+// This will basically work like a try/finally. Syntactic sugar as they call it. If an exception is thrown, it'll still dispose of it. 
+using (var writer = new FileWriter(filePath))
+{
 writer.Write("Testing this with a fairly larger string that is full of words");
 writer.Write("This should be in the second line of the text file due to the environment.newline we set earlier");
-writer.Dispose(); // We dispose of it once we're done using it, otherwise an exception is thrown due to the resource still being "open" by the writer.
+}
+// writer.Dispose(); // We dispose of it once we're done using it, otherwise an exception is thrown due to the resource still being "open" by the writer.
 
 // Had to move the creation of the reader here, otherwise it wouldn't work.
-var reader = new SpecificLineFromTextFileReader(filePath);
+// This is also a valid use of "using". Dispose at the end of the scope of the variable... Not sure exactly where.
+using var reader = new SpecificLineFromTextFileReader(filePath);
 Console.WriteLine(reader.ReadLineNumber(1));
 Console.WriteLine(reader.ReadLineNumber(2));
 Console.WriteLine(reader.ReadLineNumber(3));
 Console.WriteLine(reader.ReadLineNumber(4));
 Console.WriteLine(reader.ReadLineNumber(5));
-reader.Dispose();
+// reader.Dispose();
 
 Console.WriteLine("Press any key to close.");
 Console.ReadKey();
