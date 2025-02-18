8bb23c3052e84795b995c903bb4d1161ed27795f
LucasSM3006 <sasso3006@gmail.com>
2025-02-18 20:47:56 -0300

    Dispose method, alllinesfromtextfilereader.

---
+++ Exercise43/Exercise43/Program.cs
@@ -0,0 +1,35 @@
+﻿/*
+ * Dispose method - AllLinesFromTextFileReader
+
+Finish the implementation of the AllLinesFromTextFileReader class.
+
+    It shall be possible to use it with the using statement so that all unmanaged resources it uses are correctly disposed of.
+
+    Its _streamReader field shall be set in the constructor using the given file path.
+ */
+
+public class AllLinesFromTextFileReader : IDisposable
+{
+    private readonly StreamReader _streamReader;
+
+    public AllLinesFromTextFileReader(string filePath)
+    {
+        _streamReader = new StreamReader(filePath);
+    }
+
+    public List<string> ReadAllLines()
+    {
+        var result = new List<string>();
+        while (!_streamReader.EndOfStream)
+        {
+            result.Add(_streamReader.ReadLine());
+        }
+
+        return result;
+    }
+
+    public void Dispose()
+    {
+        _streamReader.Dispose();
+    }
+}
